932d9c47600717583c3269e96f5c1d35eb128778
Chetan Patil <132184999+Chetanp203@users.noreply.github.com>
2024-04-05 12:52:02 +0530

    nav additions (#1)
    Co-authored-by: Chetan Patil <patil.chetan199@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,38 @@
 import React, {Component} from 'react';
+import { FiShoppingCart } from "react-icons/fi";
+import { HiMiniMagnifyingGlass } from "react-icons/hi2";
+
 
 class Navbar extends Component {
     render() {
         return (
             <div className="h-24 w-full ">
-            <div>
-                <ul className="flex w-full gap-2 justify-end ">
+            <div className="h-9 px-4 py-1">
+                <ul className="flex w-full gap-3 justify-end ">
                     <li>Help</li>
                     <li>Orders & Returns</li>
-                    <li>HI,John</li>
+                    <li>Hi,John</li>
                 </ul>
             </div>
+                <div className="flex justify-between px-4">
+                    <div className="font-bold text-3xl">
+                        ECOMMERCE
+                    </div>
+                    <div>
+                        <ul className="flex gap-4">
+                            <li>Categories</li>
+                            <li>Sale</li>
+                            <li>Clearance</li>
+                            <li>New Stock</li>
+                            <li>Trending</li>
+                        </ul>
+                    </div>
+                    <div className="flex gap-4">
+                        <FiShoppingCart />
+                        <HiMiniMagnifyingGlass />
+                    </div>
+                </div>
+
             </div>
         );
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Navbar from "~/components/Navbar";
 
 export default function Home() {
   return (
- <div className="w-full">
+ <div className="w-full ">
      <Navbar/>
    hello
  </div>
