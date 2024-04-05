8c760326251ef04973f06d6e816b1f0879c62dd9
Chetan Patil <patil.chetan199@gmail.com>
2024-04-05 15:00:31 +0530

    Wip

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
-
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div className="h-24 w-full ">
-            <div className="h-9 px-4 py-1">
-                <ul className="flex w-full gap-3 justify-end ">
-                    <li>Help</li>
-                    <li>Orders & Returns</li>
-                    <li>Hi,John</li>
-                </ul>
-            </div>
-                <div className="flex justify-between px-4">
-                    <div className="font-bold text-3xl">
-                        ECOMMERCE
-                    </div>
-                    <div>
-                        <ul className="flex gap-4">
-                            <li>Categories</li>
-                            <li>Sale</li>
-                            <li>Clearance</li>
-                            <li>New Stock</li>
-                            <li>Trending</li>
-                        </ul>
-                    </div>
-                    <div className="flex gap-4">
-                        <FiShoppingCart />
-                        <HiMiniMagnifyingGlass />
-                    </div>
-                </div>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="h-24 w-full ">
+        <div className="h-9 px-6 py-1">
+          <ul className="flex w-full cursor-pointer justify-end gap-4 text-xs font-normal">
+            <li>Help</li>
+            <li>Orders & Returns</li>
+            <li>Hi,John</li>
+          </ul>
+        </div>
+        <div className="flex h-16 items-center px-6">
+          <div className=" w-1/4 cursor-pointer text-3xl font-bold">
+            ECOMMERCE
+          </div>
+          <div className="w-1/2">
+            <ul className="flex cursor-pointer justify-center gap-7">
+              <li>Categories</li>
+              <li>Sale</li>
+              <li>Clearance</li>
+              <li>New Stock</li>
+              <li>Trending</li>
+            </ul>
+          </div>
+          <div className="flex w-1/4 cursor-pointer justify-end gap-5">
+            <FiShoppingCart />
+            <HiMiniMagnifyingGlass />
+          </div>
+        </div>
+        <div className="flex h-9 items-center justify-center gap-4 bg-[#F4F4F4]">
+          <MdKeyboardArrowLeft />
+          <span>Get 10% off on business sign up</span>
+          <MdKeyboardArrowRight />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
